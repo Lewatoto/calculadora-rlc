b6c7f90f0374c8497a3b9e8bc2f2e89353802f1d
lewatoto <lewatoto@gmail.com>
2015-01-10 21:14:18 -0600

    corrigiendo problemas cache

--- js/a_cache.js
+++ js/a_cache.js
@@ -1,13 +1,7 @@
-window.addEventListener('load', function (e) {
-
-  window.applicationCache.addEventListener('updateready', function(e) {
-    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-      if (confirm('Una nueva versión de este sitio está disponible, ¿Deseas descargarla?')) {
-        window.location.reload();
-      }
-    } else {
-      // Manifest didn't changed. Nothing new to server.
-    }
-  }, false);
-
-}, false);
+function onUpdateReady() {
+  alert('Nueva versión encontrada');
+}
+window.applicationCache.addEventListener('updateready', onUpdateReady);
+if(window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+  onUpdateReady();
+}
