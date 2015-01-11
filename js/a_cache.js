4ea93a48d476c4e0001ee2d6f1afd05898f98978
lewatoto <lewatoto@gmail.com>
2015-01-11 00:02:27 -0600

    corrigiendo problemas cache

--- js/a_cache.js
+++ js/a_cache.js
@@ -1,5 +1,5 @@
 function onUpdateReady() {
-  alert('found new version!');
+  alert('Se han actualizado algunos archivos, por favor reinicia la aplicación');
 }
 window.applicationCache.addEventListener('updateready', onUpdateReady);
 if(window.applicationCache.status === window.applicationCache.UPDATEREADY) {
